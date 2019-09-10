44ba09734267dd167a8d22bd069cc71b0109a50e
Francesco Grossi <rancesco.grossi@gmail.com>
2019-09-10 15:31:06 +0200

    test

---
+++ src/main/java/mio/training/patterns/examples/App.java
@@ -0,0 +1,13 @@
+package mio.training.patterns.patternpatternExamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
